479277320dec78cefc8c732b8f64e4123c252c4f
shroukmamdoh <shroukmamdoh9@gmail.com>
2017-02-12 09:42:56 -0800

    Add login module UI

---
+++ app/modules/login/login.ctrl.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('dashboard')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = [];
+    function LoginController() {
+
+    }
+})();
---
+++ app/modules/login/login.route.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('dashboard')
+        .config(routes);
+
+    routes.$inject = ['$stateProvider'];
+    function routes($stateProvider) {
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'modules/login/login.html',
+                controller: 'LoginController as ctrl',
+                data: {
+                    portected: true
+                }
+            });
+    }
+})();
