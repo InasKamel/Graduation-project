e474e5fd2075582a92203ec923e4c720a3193d37
Amr Salama <amr.mo.salama@gmail.com>
2017-02-14 09:42:17 +0200

    Add list products functionality

--- app/modules/list-products/list-products.ctrl.js
+++ app/modules/list-products/list-products.ctrl.js
@@ -5,9 +5,23 @@
         .module('dashboard')
         .controller('ListProductsController', ListProductsController);
 
-    ListProductsController.$inject = [];
-    function ListProductsController() {
+    ListProductsController.$inject = ['$state', 'ListProductsService'];
+    function ListProductsController($state, ListProductsService) {
         var vm = this;
-        
+        vm.products = [];
+
+        init();
+
+        function init() {
+            var categoryId = $state.params.id;
+            ListProductsService
+                .getProducts(categoryId)
+                .then(function(res) {
+                    vm.products = res.data.products;
+                })
+                .catch(function(err) {
+                    // @TODO: handle the error
+                });
+        }
     }
 })();
