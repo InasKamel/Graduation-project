308f2897d4b3b0cc7c24ea838481a5bade7ad703
Amr Salama <amr.mo.salama@gmail.com>
2017-02-14 04:55:54 +0200

    Update products list page

--- app/modules/list-products/list-products.route.js
+++ app/modules/list-products/list-products.route.js
@@ -8,42 +8,13 @@
     routes.$inject = ['$stateProvider'];
     function routes($stateProvider) {
         $stateProvider
-            .state('login', {
-                url: '/login',
-                templateUrl: 'modules/login/list-products.html',
-                controller: 'ListController as ctrl',
+            .state('dashboard.list-products', {
+                url: '/list-products',
+                templateUrl: 'modules/list-products/list-products.html',
+                controller: 'ListProductsController as ctrl',
                 data: {
                     portected: true
                 }
             });
     }
 })();
-/*
-var obj = angular.module('myApp',["ngMessages"]); // brackets for the dependinces
-obj.controller('listController',['$http','$log','$scope','$timeout',function($http,$log,$scope,$timeout){
-
-
-
-
-
-
-// http request using the endpoint
-$http({
-      method: 'GET',
-      url: ''
-   }).then(function (response){
-
-       // alter this line according to the response parameters ;)
-        $scope.products = response.data.results;
-        $log.info(response.data.results);
-        
-
-   },function (error){
-
-   });
-
-
-   }]);
-    
-
-*/
