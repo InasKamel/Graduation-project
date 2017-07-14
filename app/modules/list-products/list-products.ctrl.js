36846f3cb77184d2fb53b53cad7d7070390016a8
Amr Salama <amr.mo.salama@gmail.com>
2017-07-14 03:37:12 +0200

    Refact list-products modules

--- app/modules/list-products/list-products.ctrl.js
+++ app/modules/list-products/list-products.ctrl.js
@@ -19,6 +19,9 @@
                 .getProducts(vm.categoryId)
                 .then(function(res) {
                     vm.products = res.data;
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].forEach(function (n) {
+                        vm.products.push(n);
+                    });
                 })
                 .catch(function(err) {
                     // @TODO: handle the error
